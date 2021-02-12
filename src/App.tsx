0c9ecaeb31ce35edab16e8505d2007e7333243d3
Daniel Silva <azsidaniel@gmail.com>
2021-02-11 21:02:29 -0300

    Estilizando Dashboard da aplicação.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import { FC } from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import GlobalStyle from './styles/global';
+
 import Routes from './routes';
 
 const App: FC = () => (
-  <BrowserRouter>
-    <Routes />
-  </BrowserRouter>
+  <>
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>
+    <GlobalStyle />
+  </>
 );
 
 export default App;
--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,57 @@
 import React, { FC } from 'react';
+import { FiChevronRight } from 'react-icons/fi';
+
+import logoImage from '../../assets/logo.svg';
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: FC = () => {
-  return <h1>Dashboard</h1>;
+  return (
+    <>
+      <img src={logoImage} alt="Github Explorer" />
+      <Title>Explore repositórios no Github</Title>
+
+      <Form>
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars.githubusercontent.com/u/44439702?s=460&u=37b77857e20d142439f4cd85e0f026141c2441af&v=4"
+            alt="Repositório"
+          />
+          <div>
+            <strong>azsidaniel/nodejs</strong>
+            <p>Descrição da minha biblioteca</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars.githubusercontent.com/u/44439702?s=460&u=37b77857e20d142439f4cd85e0f026141c2441af&v=4"
+            alt="Repositório"
+          />
+          <div>
+            <strong>azsidaniel/nodejs</strong>
+            <p>Descrição da minha biblioteca</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars.githubusercontent.com/u/44439702?s=460&u=37b77857e20d142439f4cd85e0f026141c2441af&v=4"
+            alt="Repositório"
+          />
+          <div>
+            <strong>azsidaniel/nodejs</strong>
+            <p>Descrição da minha biblioteca</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
